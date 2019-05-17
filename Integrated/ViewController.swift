ffe39bc8d696b847619cce361b67a866b4bdc514
Mingzhe Qiu <developer@developers-Mac.local>
2019-05-17 21:28:52 +0800

    Initial Repository

---
+++ Integrated/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Integrated
+//
+//  Created by developer on 5/17/19.
+//  Copyright © 2019 developer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
